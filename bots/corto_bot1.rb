062e5cef414fd85735233c2c3a430301cc63119b
corti <corti@corti-ThinkPad-L430.(none)>
2013-04-11 23:22:26 +0200

    corto actually shoots now

--- bots/corto_bot1.rb
+++ bots/corto_bot1.rb
@@ -3,26 +3,22 @@ class CortoBot1 < RTanque::Bot::Brain
   include RTanque::Bot::BrainHelper
 
   def tick!
-    self.random_walk
-    ## main logic goes here
-
-    # use self.sensors to detect things
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Sensors
-
-    # use self.command to control tank
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Command
-
-    # self.arena contains the dimensions of the arena
-    # See http://rubydoc.info/github/awilliams/RTanque/master/frames/RTanque/Arena
+    random_walk
+    shoot
   end
 
   def random_walk
-    command.heading = self.sensors.position.heading(RTanque::Point.new(*random_position, self.arena))
+    command.heading = sensors.position.heading(random_position)
     command.speed = random_speed
   end
 
+  def shoot
+    command.turret_heading = target_angle
+    command.fire(MIN_FIRE_POWER)
+  end
+
   def random_position
-    [Random.rand(self.arena.width), Random.rand(self.arena.height)]
+    RTanque::Point.new(Random.rand(arena.width), Random.rand(arena.height), arena)
   end
 
   def random_speed
@@ -31,5 +27,32 @@ class CortoBot1 < RTanque::Bot::Brain
 
   def min_speed
     1
+  end
+
+  def target_angle
+    RTanque::Heading.new_between_points(sensors.position, target)
+  end
+
+  def target
+    farthest_corner
+  end
+
+  def farthest_corner
+    max = { dist: 0 }
+    corners.each do |corner|
+      dist = RTanque::Point.distance(corner, sensors.position)
+      if max[:dist] < dist
+        max[:corner] = corner
+        max[:dist] = dist
+      end
+    end
+    max[:corner]
+  end
+
+  def corners
+    [RTanque::Point.new(0, 0, arena),
+     RTanque::Point.new(0, arena.height, arena),
+     RTanque::Point.new(arena.width, 0, arena),
+     RTanque::Point.new(arena.width, arena.height, arena)]
   end
 end
